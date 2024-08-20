836f7170ecdf3234b98c93cb66a6a19fa002e999
Nerdman106 <50927056+Nerdman106@users.noreply.github.com>
2024-08-20 20:00:47 +0300

    finished javascript exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = (loops) => {
+    if (loops < 0){
+        return "OOPS";
+    }else if(loops == 0){
+        return 0;
+    }
 
+    let arr = [1,1]
+    while(arr.length < loops){
+        arr.push(arr.at(-1) + arr.at(-2))
+    }
+    return arr.at(-1)
 };
+
+//console.log(fibonacci("4"))
 
 // Do not edit below this line
 module.exports = fibonacci;
